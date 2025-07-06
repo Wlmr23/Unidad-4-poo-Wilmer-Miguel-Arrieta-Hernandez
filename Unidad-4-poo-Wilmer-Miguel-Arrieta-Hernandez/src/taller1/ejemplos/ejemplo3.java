6248eae89a71c739ef0665cb1aacf598e3f9e40b
Wlmr23 <wilmermiguelarrietahernandez@gmail.com>
2025-07-06 13:20:01 -0500

    feat: wilmer arrieta - ejemplo 3 taller 1

---
+++ Unidad-4-poo-Wilmer-Miguel-Arrieta-Hernandez/src/taller1/ejemplos/ejemplo3.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller1.ejemplos;
+
+/**
+ *
+ * @author WILMER Y ANDREA
+ */
+public class ejemplo3 {
+    public class Coche {
+String marca;
+// Constructor parametrizado 
+public Coche(String marca) {
+    this.marca = marca;
+        }
+    }
+}
