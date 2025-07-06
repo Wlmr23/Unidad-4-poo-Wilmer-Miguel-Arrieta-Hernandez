74934eba98315250621c35487d9c6b8c149bac70
Wlmr23 <wilmermiguelarrietahernandez@gmail.com>
2025-07-06 13:19:25 -0500

    feat: wilmer arrieta - ejemplo 2 taller 1

---
+++ Unidad-4-poo-Wilmer-Miguel-Arrieta-Hernandez/src/taller1/ejemplos/ejemplo2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller1.ejemplos;
+
+/**
+ *
+ * @author WILMER Y ANDREA
+ */
+public class ejemplo2 {
+        //2. Tipos de Constructores en Java
+
+    
+public class Coche { String marca;
+
+
+// Constructor por defecto
+public Coche() {
+marca = "Desconocida";
+    }
+
+    }
+
+}
