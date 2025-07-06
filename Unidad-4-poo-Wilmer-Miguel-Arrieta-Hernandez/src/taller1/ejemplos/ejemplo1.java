a5a7688f9abbd6aad1c4e126899ac4ac748cbeff
Wlmr23 <wilmermiguelarrietahernandez@gmail.com>
2025-07-06 13:18:39 -0500

    feat: wilmer arrieta - ejemplo 1 taller 1

---
+++ Unidad-4-poo-Wilmer-Miguel-Arrieta-Hernandez/src/taller1/ejemplos/ejemplo1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller1.ejemplos;
+
+/**
+ *
+ * @author WILMER Y ANDREA
+ */
+public class ejemplo1 {
+    //1. Ejemplo de Constructor Simple:
+    public class Persona { 
+        String nombre;
+
+// Constructor
+public Persona() {
+nombre = "Sin nombre";
+
+        }
+    }
+}
