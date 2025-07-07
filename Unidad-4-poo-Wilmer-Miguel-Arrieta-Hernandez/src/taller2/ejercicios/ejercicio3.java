fc3e6820c21fd01ce8745e36e49112d73df6f88b
Wlmr23 <wilmermiguelarrietahernandez@gmail.com>
2025-07-06 22:45:43 -0500

    feat: wilmer arrieta - ejercicio 3 taller 2

---
+++ Unidad-4-poo-Wilmer-Miguel-Arrieta-Hernandez/src/taller2/ejercicios/ejercicio3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller2.ejercicios;
+
+/**
+ *
+ * @author WILMER Y ANDREA
+ */
+public class ejercicio3 {
+    private String mensaje = "hola mundo";
+    
+    public static void mostrarmensaje(){
+        System.out.println(this.mensaje);
+    }
+}
