d693356d3770c98459f37f92ef14e2d7eb719ae4
Wlmr23 <wilmermiguelarrietahernandez@gmail.com>
2025-07-07 22:51:07 -0500

    feat: wilmer arrieta - ejercicio 2 taller 3

---
+++ Unidad-4-poo-Wilmer-Miguel-Arrieta-Hernandez/src/taller3/ejercicios/Matematicas.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller3.ejercicios;
+
+/**
+ *
+ * @author WILMER Y ANDREA
+ */
+public class Matematicas {
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public static int restar(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static double dividir(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+        return (double) a / b;
+    }
+}
+
