8674d0cd8d86b5001c613ba5659f08c765854408
Wlmr23 <wilmermiguelarrietahernandez@gmail.com>
2025-07-08 00:11:58 -0500

    feat: wilmer arrieta - ejemplo taller 9

---
+++ Unidad-4-poo-Wilmer-Miguel-Arrieta-Hernandez/src/taller9/ejemplos/ejemplo2.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller9.ejemplos;
+
+/**
+ *
+ * @author WILMER Y ANDREA
+ */
+public class ejemplo2 {
+    
+
+
+public class Empleado {
+protected String nombre;
+
+
+public void mostrarNombre() {
+System.out.println("Nombre: " + nombre);
+}
+}
+
+
+public class Gerente extends Empleado {
+private String departamento;
+
+
+@Override
+public void mostrarNombre() {
+super.mostrarNombre(); // Llama al método de la clase base
+System.out.println("Departamento: " + departamento);
+}
+}
+public class Vehiculo {
+protected String marca;
+
+
+public Vehiculo(String marca) {
+this.marca = marca;
+}
+}
+
+
+public class Coche extends Vehiculo {
+private int numeroDePuertas;
+
+
+public Coche(String marca, int numeroDePuertas) { 
+
+
+
+super(marca); // Llama al constructor de la clase base
+this.numeroDePuertas = numeroDePuertas;
+}
+}
+
+}
