69d096ac44138204bd776f4145bb8e853712ca72
Wlmr23 <wilmermiguelarrietahernandez@gmail.com>
2025-07-07 22:55:34 -0500

    feat: wilmer arrieta - ejercicio 3 taller 3

---
+++ Unidad-4-poo-Wilmer-Miguel-Arrieta-Hernandez/src/taller3/ejercicios/PruebaMatematicas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller3.ejercicios;
+/**
+ *
+ * @author WILMER Y ANDREA
+ */
+public class PruebaMatematicas {
+    
+    public static void main(String[] args) {
+        System.out.println("Suma: " + Matematicas.sumar(5, 3));
+        System.out.println("Resta: " + Matematicas.restar(5, 3));
+        System.out.println("Multiplicación: " + Matematicas.multiplicar(5, 3));
+        System.out.println("División: " + Matematicas.dividir(10, 2));
+    }
+}
+
